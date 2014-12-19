9aaae17dde60a99c7c37bc611038f53b8bfdbc40
Sergio Gomez <sergomez@google.com>
2014-12-19 12:13:02 +0000

    Update images and add test

---
+++ test/test.lua
@@ -0,0 +1,49 @@
+local paths = require 'paths'
+require 'image'
+
+local totem = require 'totem'
+
+local cairo = require 'oocairo'
+local plplot = require 'plplot'
+
+
+local WIDTH  = 800
+local HEIGHT = 800
+
+
+local tester = totem.Tester()
+local tests = {}
+
+
+function tests.testLine()
+  -- Cairo initialization
+  local cairoSurface = cairo.image_surface_create("rgb24", WIDTH, HEIGHT)
+  local cairoContext = cairo.context_create(cairoSurface)
+  cairoContext:set_source_rgb(1, 1, 1)
+  cairoContext:paint()
+
+  -- PLplot
+  local x = torch.DoubleTensor{0,1,2,3,4,5,6,7,8,9,10}
+
+  local plots = {}
+  for i = 1,3 do
+      plots[i] = {x, torch.mul(x,i), '*-'}
+  end
+
+  plplot.init(cairoContext, WIDTH, HEIGHT)
+  plplot.plot(plots)
+  plplot.labels('Line Plot', 'X axis', 'Y axis')
+  plplot.close()
+
+  -- Render cairo surface as a tensor
+  local result = cairo.rgb2tensor(cairoSurface, false):double():div(255)
+
+  -- Open reference PNG
+  local pathReference = paths.thisfile('../img/line.png')
+  local reference = image.load(pathReference)
+
+  tester:assertTensorEq(result, reference, 1e-15, 'Different from reference')
+end
+
+
+return tester:add(tests):run()
